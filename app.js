c6e389c4338faf306bfc696db684392cbdda911b
Wajahat <wajahatalimay2001@gmail.com>
2021-07-20 09:26:43 -0700

    ok

--- app.js
+++ app.js
@@ -1,55 +1,93 @@
-var inp=document.getElementsByTagName('button')
-console.log(inp)
-function btnClicked(ab){
-    if(ab==1){
-        
-    }
-    else if(ab==2){
-        
-    }
-    else if(ab==3){
-        
-    }
-    else if(ab=='plus'){
-        
-    }
-    else if(ab==4){
-        
-    }
-    else if(ab==5){
-        
-    }
-    else if(ab==6){
-        
-    }
+// var inp=document.getElementById('inp')
+// console.log(inp)
+// function btnClicked(ab){
+//     if(ab==1){
+//         inp.value+=ab;
+//     }
+//     else if(ab==2){
+//         inp.value+=ab;
+//     }
+//     else if(ab==3){
+//         inp.value+=ab;
+//     }
+//     else if(ab=='+'){
+//         console.log('It is plus')
+//         inp.value += '+' ;
+//     }
+//     else if(ab==4){
+//         inp.value+=ab;
+//     }
+//     else if(ab==5){
+//         inp.value+=ab;
+//     }
+//     else if(ab==6){
+//         inp.value+=ab;
+//     }
 
-    else if(ab='minus'){
-        
-    }
-    else if(ab==7){
-        
-    }
-    else if(ab==8){
-        
-    }
+//     else if(ab='-'){
+//         inp.value+= '-';
+//     }
+//     else if(ab==7){
+//         inp.value+=ab;
+//     }
+//     else if(ab==8){
+//         inp.value+=ab;
+//     }
 
-    else if(ab==9){
-        
-    }
-    else if(ab=='multiply'){
-        
-    }
-    else if(ab==0){
-        
-    }
-    else if(ab=='dot'){
-        
-    }
-    else if(ab=='equals'){
-        
-    }
-    else if(ab=='divide'){
-        
-    }
+//     else if(ab==9){
+//         inp.value+=ab;
+//     }
+//     else if(ab=='*'){
+//         inp.value+='*';
+//     }
+//     else if(ab==0){
+//         inp.value+=ab;
+//     }
+//     else if(ab=='.'){
+//         inp.value+='.';
+//     }
+//     // else if(ab=='='){
+//     //     inp.value+='=';
+//     // }
+//     else if(ab=='/'){
+//         inp.value+='/';
+//     }
 
-}
+// }
+
+
+// var a=+(document.getElementById('inp').value);
+// function compute(){
+//     var result=eval(a);
+//     inp.value=result;
+// }
+
+// select all the buttons
+const buttons = document.querySelectorAll('button');
+// select the <input type="text" class="display" disabled> element
+const display = document.querySelector('.display');
+
+// add eventListener to each button
+buttons.forEach(function(button) {
+    button.addEventListener('click', calculate);
+  });
+
+  // calculate function
+function calculate(event) {
+    // current clicked buttons value
+    const clickedButtonValue = event.target.value;
+  
+    if (clickedButtonValue === '=') {
+      // check if the display is not empty then only do the calculation
+      if (display.value !== '') {
+        // calculate and show the answer to display
+        display.value = eval(display.value);
+      }
+    } else if (clickedButtonValue === 'C') {
+      // clear everything on display
+      display.value = '';
+    } else {
+      // otherwise concatenate it to the display
+      display.value += clickedButtonValue;
+    }
+  }
